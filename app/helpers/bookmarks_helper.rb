95fa7908beb2a8eff1ac4fee2cafdb410e8e863d
Thomas Uselman <thomasuselman@gmail.com>
2011-12-17 21:12:43 -0600

    initial commit

---
+++ app/helpers/bookmarks_helper.rb
@@ -0,0 +1,28 @@
+module BookmarksHelper
+  
+  def current_user?(user)
+    user == current_user
+    cookies.permanent.signed[:bookmark_token] = [user.id, bookmark.id]
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def get_all_users_for_bookmark(bookmark)
+    #users = []
+    #bookmark.users.each do |user|
+    #  users << user.username
+    #end
+    #users.join(",")
+    bookmark.users.map(&:username).join(", ")
+  end
+  
+  
+  private
+  
+    def bookmark_token
+      cookies.signed[:bookmark_token] || [nil, nil]
+    end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+BookmarkProject1::Application.routes.draw do
+  get "sessions/new"
+
+  resources :bookmarks
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
+  match "/about" => "users#help"
+  match "/contact" => "users#contact"
+  
+  match "/login" => "users#show"
+  match "/display/:id" => "bookmarks#display"
+  match "/image" => "users#upload"
+  match "/logout" => "users#logout"
+  match "/edit" => "users#edit"
+  match "/help" => "users#help"
+  match "/bookmark" => "bookmark#show"
+  match "/bookmarkedit" => "bookmark#edit"
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  root :to => "sessions#new"
+end
